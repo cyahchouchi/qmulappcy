49da15ab2a01ad16462231534e475b0bb874b88a
Christopher Yahchouchi <cy302@itl208.student.eecs.qmul.ac.uk>
2017-03-07 12:20:45 +0000

    sentiment img deployment

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Your App Name',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='Chris',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['MarkupSafe', 'Flask','flask-wtf','flask-babel','markdown','flup'],
