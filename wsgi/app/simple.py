f94c50666afec149945e3c0826649bfb35b92b65
CEY <mbp@CEY.local>
2017-03-31 18:22:11 +0100

    arman

--- wsgi/app/simple.py
+++ wsgi/app/simple.py
@@ -1,11 +1,5 @@
-'''This example demonstrates embedding a standalone Bokeh document
-into a simple Flask application, with a basic HTML web form.
-To view the example, run:
-    python simple.py
-in this directory, and navigate to:
-    http://localhost:5000
-'''
 import flask
+from app import app
 
 from bokeh.embed import components
 from bokeh.plotting import figure
