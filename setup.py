03eb977cc9ea6f04499649b10bb3ade8f6e40741
CEY <mbp@CEY.local>
2017-03-31 18:15:56 +0100

    arman

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Your App Name',
+setup(name='Sentiments',
       version='1.0',
       description='OpenShift App',
-      author='Chris',
-      author_email='example@example.com',
+      author='Christopher Yahchouchi',
+      author_email='chrisyahchouchi@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['MarkupSafe', 'Flask','flask-wtf','flask-babel','markdown','flup','unirest', 'bokeh'],
       )
